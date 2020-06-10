e8f7b8b312d01dbe6212e4e22ed5b71d49a57371
Семья <iogannbillE@gmail.com>
2020-06-11 01:40:06 +0600

    Update homework

--- src/Matrix.java
+++ src/Matrix.java
@@ -2,117 +2,139 @@
 
 public class Matrix {
     //поля класса
-    int n, m;
-    int massive[][] = new int[n][m];
+    int n = 0, m = 0;
+    int massive[][];
 
     //перегруженный конструктор
-    Matrix(int n, int m, int massive[][]) {
-        this.n = n;
-        this.m = m;
-        this.massive[n][m] = massive[n][m];
+    Matrix(int massive[][]) {
+        this.massive = massive;
+        this.n = massive.length;
+        this.m = massive[0].length;
+
     }
 
     //конструктор по-умолчанию
     Matrix() {
         this.n = 2;
         this.m = 2;
+        this.massive = new int[n][m];
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public int getM() {
+        return m;
     }
 
     //сложение
-    void summa(Matrix matrixA, Matrix matrixB, Matrix matrixResult, int i, int j) {
-        matrixResult.n = Math.max(matrixA.n, matrixB.n);
-        matrixResult.m = Math.max(matrixA.m, matrixB.m);
-        matrixResult.massive = new int[matrixResult.n][matrixResult.m];
-        for (i = 0; i < matrixResult.n; i++)
-            for (j = 0; j < matrixResult.m; j++) {
-                matrixResult.massive[i][j] = matrixA.massive[i][j] + matrixB.massive[i][j];
+    Matrix summa(Matrix matrixA) {
+        Matrix matrixResult = new Matrix(new int[this.getN()][this.getM()]);
+        for (int i = 0; i < matrixResult.n; i++){
+            for (int j = 0; j < matrixResult.m; j++) {
+                matrixResult.massive[i][j] = this.massive[i][j] + matrixA.massive[i][j];
             }
+        }
+        return matrixResult;
     }
 
     //вычитание
-    void minus(Matrix matrixA, Matrix matrixB, Matrix matrixResult, int i, int j) {
-        matrixResult.n = Math.max(matrixA.n, matrixB.n);
-        matrixResult.m = Math.max(matrixA.m, matrixB.m);
-        matrixResult.massive = new int[matrixResult.n][matrixResult.m];
-        for (i = 0; i < matrixResult.n; i++)
-            for (j = 0; j < matrixResult.m; j++) {
-                matrixResult.massive[i][j] = matrixA.massive[i][j] - matrixB.massive[i][j];
+    Matrix minus(Matrix matrixA) {
+        Matrix matrixResult = new Matrix(new int[this.getN()][this.getM()]);
+        for (int i = 0; i < matrixResult.n; i++){
+            for (int j = 0; j < matrixResult.m; j++) {
+                matrixResult.massive[i][j] = this.massive[i][j] - matrixA.massive[i][j];
             }
+        }
+        return matrixResult;
     }
 
     //вывод матрицы
-    void printMatrix(Matrix matrix) {
-        for (int i = 0; i < matrix.n; i++) {
-            for (int j = 0; j < matrix.m; j++) {
-                System.out.print(" " + matrix.massive[i][j] + " ");
+    void printMatrix() {
+        for (int i = 0; i < this.getN(); i++) {
+            for (int j = 0; j < this.getM(); j++) {
+                System.out.print(" " + this.massive[i][j] + " ");
             }
             System.out.println();
         }
     }
 
     //инверсия матрицы
-    void invers(Matrix matrixA, Matrix matrixResult, int i, int j) {
-        for (i = 0; i < n; i++)
-            for (j = 0; j < m; j++) {
-                matrixResult.massive[i][j] = matrixA.massive[j][i];
+    Matrix invers() {
+        Matrix matrixResult = new Matrix(new int[this.getN()][this.getM()]);
+        for (int i = 0; i < this.getN(); i++){
+            for (int j = 0; j < this.getM(); j++) {
+                matrixResult.massive[i][j] = this.massive[j][i];
             }
+        }
+        return matrixResult;
     }
 
     //вывод единичной матрицы
-    void onceMatrix(Matrix matrixResult, int i, int j) {
-        for (i = 0; i < n; i++)
-            for (j = 0; j < m; j++) {
+    static Matrix onceMatrix(int r) {
+        Matrix matrixResult = new Matrix(new int[r][r]);
+        for (int i = 0; i < matrixResult.n; i++){
+            for (int j = 0; j < matrixResult.m; j++) {
                 if (i == j)
                     matrixResult.massive[i][j] = 1;
                 else matrixResult.massive[i][j] = 0;
             }
+        }
+        return matrixResult;
     }
 
     //вывод геттеров
-    void printGetter(Matrix matrixResult) {
-        System.out.println("Количество строк в матрице A:" + matrixResult.n);
-        System.out.println("Количество столбцов в матрице A:" + matrixResult.m);
+    void printGetter() {
+        System.out.println("Количество строк в матрице A:" + this.n);
+        System.out.println("Количество столбцов в матрице A:" + this.m);
     }
 
     //умножение на матрицу
-    void multiplicationByMatrix(Matrix matrixA, Matrix matrixB, Matrix matrixResult, int i, int j, int k) {
-        matrixResult.n = matrixA.n;
-        matrixResult.m = matrixB.m;
-        for (i = 0; i < matrixResult.n; i++)
-            for (j = 0; j < matrixResult.m; j++)
-                for (k = 0; k < matrixResult.n; k++) {
-                    matrixResult.massive[i][j] += matrixA.massive[i][k] * matrixB.massive[k][j];
+    Matrix multiplicationByMatrix(Matrix matrixA) {
+        Matrix matrixResult = new Matrix(new int[this.n][matrixA.m]);
+        for (int i = 0; i < matrixResult.n; i++){
+            for (int j = 0; j < matrixResult.m; j++){
+                for (int k = 0; k < matrixResult.n; k++) {
+                    matrixResult.massive[i][j] += this.massive[i][k] * matrixA.massive[k][j];
                 }
+            }
+        }
+        return matrixResult;
     }
 
     //умножение на число
-    void multiplicationByNumber(Matrix matrixA, Matrix matrixResolt, int i, int j, int k) {
-        for (i = 0; i < matrixResolt.n; i++)
-            for (j = 0; j < matrixResolt.m; j++)
-                matrixResolt.massive[i][j] = matrixA.massive[i][j] * k;
-    }
-
-    //обнуление матрицы
-    void zero(Matrix matrix, int i, int j) {
-        for (i = 0; i < matrix.n; i++)
-            for (j = 0; j < matrix.m; j++)
-                matrix.massive[i][j] = 0;
+    Matrix multiplicationByNumber(int k) {
+        Matrix matrixResult = new Matrix(new int[this.n][this.m]);
+        for (int i = 0; i < matrixResult.n; i++){
+            for (int j = 0; j < matrixResult.m; j++){
+                matrixResult.massive[i][j] = this.massive[i][j] * k;
+            }
+        }
+            return matrixResult;
     }
 
     //квадрат матрицы
-    void square(Matrix matrixA, Matrix matrixResult, int i, int j, int k) {
-        matrixResult.n = matrixA.n;
-        matrixResult.m = matrixA.m;
-        for (i = 0; i < matrixResult.n; i++)
-            for (j = 0; j < matrixResult.m; j++)
-                for (k = 0; k < matrixResult.n; k++)
-                    matrixResult.massive[i][j] += matrixA.massive[i][k] * matrixA.massive[k][j];
+    Matrix square() {
+        Matrix matrixResult = new Matrix(new int [this.getN()][this.getM()]);
+        for (int i = 0; i < matrixResult.n; i++){
+            for (int j = 0; j < matrixResult.m; j++){
+                for (int k = 0; k < matrixResult.n; k++){
+                    matrixResult.massive[i][j] += this.massive[i][k] * this.massive[k][j];
+                }
+            }
+        }
+        return matrixResult;
     }
 
     //куб матрицы
-    void cube(Matrix matrixA, Matrix matrixSquare, Matrix matrixResult, int i, int j, int k) {
-        matrixA.multiplicationByMatrix(matrixA, matrixA, matrixResult, 0, 0, 0);
-        matrixSquare = matrixResult;
-        matrixResult.multiplicationByMatrix(matrixA, matrixSquare, matrixResult, 0, 0, 0);
+    Matrix cube() {
+        Matrix matrixFirst = new Matrix(new int[this.n][this.m]);
+        matrixFirst.massive = this.massive;
+        Matrix matrixSquare = new Matrix(new int[this.n][this.m]);
+        matrixSquare = matrixFirst.square();
+        Matrix matrixResult = new Matrix(new int[this.n][this.m]);
+        matrixResult = matrixSquare.multiplicationByMatrix(matrixFirst);
+        return matrixResult;
     }
 }
--- src/MatrixCalculate.java
+++ src/MatrixCalculate.java
@@ -2,77 +2,55 @@ public class MatrixCalculate {
 
     public static void main(String[] args) {
         Matrix matrixA = new Matrix();
-        matrixA.n = 2;
-        matrixA.m = 2;
-        matrixA.massive = new int[][]{{1, 2}, {4, 5}};
+        matrixA.n = 3;
+        matrixA.m = 3;
+        matrixA.massive = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
         Matrix matrixB = new Matrix();
-        matrixB.n = 2;
-        matrixB.m = 2;
-        matrixB.massive = new int[][]{{1, 2}, {1, 2}};
-
-        Matrix matrixResult = new Matrix();
-        Matrix matrixSquare = new Matrix();
+        matrixB.n = 3;
+        matrixB.m = 3;
+        matrixB.massive = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
         System.out.println("Матрица А:");
-        matrixA.printMatrix(matrixA);
+        matrixA.printMatrix();
         System.out.println();
 
         System.out.println("Матрица B:");
-        matrixB.printMatrix(matrixB);
+        matrixB.printMatrix();
         System.out.println();
-
-        matrixA.summa(matrixA, matrixB, matrixResult, 0, 0);
 
         System.out.println("Сумма матриц А и В:");
-        matrixResult.printMatrix(matrixResult);
+        matrixA.summa(matrixB).printMatrix();
         System.out.println();
 
-        matrixA.minus(matrixA, matrixB, matrixResult, 0, 0);
-
-        System.out.println("Разница матриц А и В:");
-        matrixResult.printMatrix(matrixResult);
+        matrixA.minus(matrixB).printMatrix();
         System.out.println();
-
-        matrixA.invers(matrixA, matrixResult, 0, 0);
 
         System.out.println("Инвертированная матрица А:");
-        matrixResult.printMatrix(matrixResult);
+        matrixA.invers().printMatrix();
         System.out.println();
-
-        matrixResult.onceMatrix(matrixResult, 0, 0);
 
         System.out.println("Единичная матрица:");
-        matrixResult.printMatrix(matrixResult);
+        Matrix.onceMatrix(5).printMatrix();
         System.out.println();
 
-        matrixA.printGetter(matrixA);
+        matrixA.printGetter();
         System.out.println();
-
-        matrixA.multiplicationByMatrix(matrixA, matrixB, matrixResult, 0, 0, 0);
 
         System.out.println("Произведение матриц А на матрицу В:");
-        matrixResult.printMatrix(matrixResult);
+        matrixA.multiplicationByMatrix(matrixB).printMatrix();
         System.out.println();
-
-        matrixA.multiplicationByNumber(matrixA, matrixResult, 0, 0, 5);
 
         System.out.println("Произведение матрицы А на 5:");
-        matrixResult.printMatrix(matrixResult);
+        matrixA.multiplicationByNumber(5).printMatrix();
         System.out.println();
-        matrixResult.zero(matrixResult, 0, 0);
-
-        matrixA.square(matrixA, matrixResult, 0, 0, 0);
 
         System.out.println("Квадрат матрицы А:");
-        matrixResult.printMatrix(matrixResult);
+        matrixA.square().printMatrix();
         System.out.println();
-        matrixResult.zero(matrixResult, 0, 0);
-
-        matrixA.cube(matrixA, matrixSquare, matrixResult, 0, 0, 0);
 
         System.out.println("Куб матрицы А:");
-        matrixResult.printMatrix(matrixResult);
+        matrixA.cube().printMatrix();
         System.out.println();
     }
 }
